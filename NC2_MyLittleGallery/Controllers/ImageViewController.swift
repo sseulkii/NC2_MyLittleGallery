ebc859ab4c1b82c358fb36d0f16b65a66c29f251
sseulkii <operry126@gmail.com>
2022-09-01 19:08:49 +0900

    create new files, build skeleton

---
+++ NC2_MyLittleGallery/Controllers/ImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  NC2_MyLittleGallery
+//
+//  Created by Seulki Lee on 2022/09/01.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NC2_MyLittleGallery/Model/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  NC2_MyLittleGallery
+//
+//  Created by Seulki Lee on 2022/09/01.
+//
+
+import Foundation
+
+class K {
+    
+    let segueShowImage = "showImage"
+    let segueShowOnWall = "showOnWall"
+    
+    let cassatt = "Cassatt"
+    let monet = "Monet"
+    let renoir = "Renoir"
+    let degas = "Degas"
+    let manet = "Manet"
+    let pissaro = "Pissaro"
+    let morisot = "Morisot"
+    let sisley = "Sisley"
+    
+}
