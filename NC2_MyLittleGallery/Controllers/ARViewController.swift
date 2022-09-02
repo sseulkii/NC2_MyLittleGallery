4823d60eaf9fe38605eb5448999cce7c61b1f7a2
sseulkii <operry126@gmail.com>
2022-09-02 09:57:08 +0900

    set arview, add save button and segue to arview

--- NC2_MyLittleGallery/Controllers/ARViewController.swift
+++ NC2_MyLittleGallery/Controllers/ARViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import ARKit
 
-class ARViewController: UIViewController {
+// https://stackoverflow.com/questions/51888939/place-image-from-gallery-on-a-wall-using-arkit
+class ARViewController: UIViewController, ARSCNViewDelegate {
+    
+    var imageToShow: UIImage?
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let arSession = ARSession()
+    let configuration = ARWorldTrackingConfiguration()
+    var additionalNode: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        sceneView.session = arSession
+        sceneView.delegate = self
+        configuration.planeDetection = .vertical
+        arSession.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if additionalNode == nil {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else {
+                return
+            }
+            
+            let width = CGFloat(imageToShow!.size.width * 0.002)
+            let height = CGFloat(imageToShow!.size.height * 0.002)
+            
+            additionalNode = SCNNode(geometry: SCNPlane(width: width, height: height))
+            
+            additionalNode?.eulerAngles.x = -.pi / 2
+            
+            additionalNode?.geometry?.firstMaterial?.diffuse.contents = imageToShow
+            
+            node.addChildNode(additionalNode!)
+        }
+        
     }
-    */
 
 }
--- NC2_MyLittleGallery/Controllers/ImageViewController.swift
+++ NC2_MyLittleGallery/Controllers/ImageViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    let k = K()
     var test = ""
     var imageSent: UIImage?
 
@@ -21,5 +22,35 @@ class ImageViewController: UIViewController {
         
         print(test)
     }
-
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+        if let imageToSave = imageSent {
+            UIImageWriteToSavedPhotosAlbum(imageToSave, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if error != nil {
+            let alert = UIAlertController(title: "Error", message: "There was an error saving your image.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Image Saved", message: "Your image has been saved in your photo library.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func showOnWall(_ sender: Any) {
+        performSegue(withIdentifier: k.segueShowOnWall, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == k.segueShowOnWall {
+            let destinationVC = segue.destination as! ARViewController
+            if let image = imageSent {
+                destinationVC.imageToShow = image
+            }
+        }
+    }
 }
